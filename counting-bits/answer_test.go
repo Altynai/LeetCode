f8530f320cafcdcbdfee378bf31c49d5832de441
Altynai <Altynai.me@gmail.com>
2016-03-19 21:31:03 +0800

    Fix golang source file format

--- counting-bits/answer_test.go
+++ counting-bits/answer_test.go
@@ -1,14 +1,13 @@
-
 package main
 
 import "testing"
 
 func TestCountBits(t *testing.T) {
-    expected := []int{0, 1, 1, 2, 1, 2}
-    result := countBits(5)
-    for i := 0; i < len(expected); i++ {
-        if result[i] != expected[i] {
-            t.Fatalf("Expected %d, got %d", expected[i], result[i])
-        }
-    }
+	expected := []int{0, 1, 1, 2, 1, 2}
+	result := countBits(5)
+	for i := 0; i < len(expected); i++ {
+		if result[i] != expected[i] {
+			t.Fatalf("Expected %d, got %d", expected[i], result[i])
+		}
+	}
 }
--- rotate-array/answer.go
+++ rotate-array/answer.go
@@ -1,24 +1,23 @@
-
 package main
 
 func rotateRange(nums []int, low, high int) {
-    var swap int
-    for ; low < high; {
-        swap = nums[low]
-        nums[low] = nums[high - 1]
-        nums[high - 1] = swap
-        low++
-        high--
-    }
+	var swap int
+	for low < high {
+		swap = nums[low]
+		nums[low] = nums[high-1]
+		nums[high-1] = swap
+		low++
+		high--
+	}
 }
 
-func rotate(nums []int, k int)  {
-    count := len(nums)
-    if k %= count; k == 0 {
-        return
-    } else {
-        rotateRange(nums, 0, count - k)
-        rotateRange(nums, count - k, count)
-        rotateRange(nums, 0, count)
-    }
+func rotate(nums []int, k int) {
+	count := len(nums)
+	if k %= count; k == 0 {
+		return
+	} else {
+		rotateRange(nums, 0, count-k)
+		rotateRange(nums, count-k, count)
+		rotateRange(nums, 0, count)
+	}
 }
