46177a46f3bae806ee5272377f7bcddd710d23c4
Jiapeng Chen <jiapeng.chen@uber.com>
2021-05-09 19:41:19 +0200

    Add Solution: Minimum Distance to the Target Element

---
+++ minimum-distance-to-the-target-element/answer.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+  int getMinDistance(vector<int> &nums, int target, int start) {
+    int n = nums.size();
+    int ans = n + start;
+    for (int i = 0; i < n; i++) {
+      if (nums[i] == target)
+        ans = min(ans, abs(i - start));
+    }
+    return ans;
+  }
+};
