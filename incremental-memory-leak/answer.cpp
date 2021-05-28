4068dde34a058df725a3ed9f194acc6b0bceae0c
Jiapeng Chen <jiapeng.chen@uber.com>
2021-05-28 10:23:17 +0200

    Add Solution: Incremental Memory Leak

---
+++ incremental-memory-leak/answer.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+  vector<int> memLeak(int memory1, int memory2) {
+    int i = 1;
+    for (; i <= max(memory1, memory2); i++) {
+      if (memory1 >= memory2) {
+        memory1 -= i;
+      } else {
+        memory2 -= i;
+      }
+    }
+    return {i, memory1, memory2};
+  }
+};
