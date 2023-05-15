d188802f83c017222b70a75fde4ecfd3ea2afd96
Jiapeng Chen <jiapeng.chen@uber.com>
2023-05-15 13:27:31 +0200

    Add Solution: Power of Heroes

---
+++ power-of-heroes/answer.cpp
@@ -0,0 +1,146 @@
+template <int MOD_> struct modnum {
+  typedef long long int64;
+
+  static constexpr int MOD = MOD_;
+  static_assert(MOD_ > 0, "MOD must be positive");
+
+private:
+  int v;
+  static int minv(int a, int m) {
+    a %= m;
+    assert(a);
+    return a == 1 ? 1 : int(m - int64(minv(m, a)) * int64(m) / a);
+  }
+
+public:
+  modnum() : v(0){};
+  modnum(int64 v_) : v(int(v_ % MOD)) {
+    if (v < 0)
+      v += MOD;
+  }
+  explicit operator int() const { return v; }
+  friend std::ostream &operator<<(std::ostream &out, const modnum &n) {
+    return out << int(n);
+  }
+  friend std::istream &operator>>(std::istream &in, modnum &n) {
+    int64 v_;
+    in >> v_;
+    n = modnum(v_);
+    return in;
+  }
+  friend bool operator==(const modnum &a, const modnum &b) {
+    return a.v == b.v;
+  }
+  friend bool operator!=(const modnum &a, const modnum &b) {
+    return a.v != b.v;
+  }
+
+  modnum inv() const {
+    modnum res;
+    res.v = minv(v, MOD);
+    return res;
+  }
+  friend modnum inv(const modnum &m) { return m.inv(); }
+  modnum neg() const {
+    modnum res;
+    res.v = v ? MOD - v : 0;
+    return res;
+  }
+  friend modnum neg(const modnum &m) { return m.neg(); }
+
+  modnum pow(int64 m) {
+    assert(m >= 0);
+    modnum res = 1, p = modnum(*this);
+    while (m) {
+      if (m & 1)
+        res *= p;
+      p *= p;
+      m /= 2;
+    }
+    return res;
+  }
+
+  modnum operator-() const { return neg(); }
+  modnum operator+() const { return modnum(*this); }
+
+  modnum &operator++() {
+    ++v;
+    if (v == MOD)
+      v = 0;
+    return *this;
+  }
+  modnum &operator--() {
+    if (v == 0)
+      v = MOD;
+    --v;
+    return *this;
+  }
+  modnum &operator+=(const modnum &o) {
+    v += o.v;
+    if (v >= MOD)
+      v -= MOD;
+    return *this;
+  }
+  modnum &operator-=(const modnum &o) {
+    v -= o.v;
+    if (v < 0)
+      v += MOD;
+    return *this;
+  }
+  modnum &operator*=(const modnum &o) {
+    v = int(int64(v) * int64(o.v) % MOD);
+    return *this;
+  }
+  modnum &operator/=(const modnum &o) { return *this *= o.inv(); }
+
+  friend modnum operator++(modnum &a, int) {
+    modnum r = a;
+    ++a;
+    return r;
+  }
+  friend modnum operator--(modnum &a, int) {
+    modnum r = a;
+    --a;
+    return r;
+  }
+  friend modnum operator+(const modnum &a, const modnum &b) {
+    return modnum(a) += b;
+  }
+  friend modnum operator-(const modnum &a, const modnum &b) {
+    return modnum(a) -= b;
+  }
+  friend modnum operator*(const modnum &a, const modnum &b) {
+    return modnum(a) *= b;
+  }
+  friend modnum operator/(const modnum &a, const modnum &b) {
+    return modnum(a) /= b;
+  }
+};
+using mint = modnum<1000000007>;
+// using mint = modnum<998244353>;
+
+class Solution {
+public:
+  int sumOfPower(vector<int> &a) {
+    int n = a.size();
+    sort(a.begin(), a.end());
+
+    // b[n - 1] = a[n-1]^3
+    // b[n - 2] = a[n-2]^3 + a[n-2] * a[n-1]^2
+    // b[n - 3] = a[n-3]^3 + a[n-3] * a[n-2]^2 + a[n-3] * a[n-1]^2 * 2^1
+    //          = (a[n-3]^2 + a[n-2]^2 + a[n-1]^2 * 2^1) * a[n-3]
+    // b[n - 4] = a[n-4]^3 + a[n-4] * a[n-3]^2 + a[n-4] * a[n-2]^2 * 2^1
+    //          = (a[n-4]^2 + a[n-3]^2 + a[n-2]^2 * 2^1) * a[n-4]
+    mint sum = mint(a[n - 1]) * a[n - 1] * a[n - 1];
+    mint ans = sum;
+    for (int i = n - 2; i >= 0; i--) {
+      sum /= a[i + 1];
+      sum *= 2;
+      sum -= mint(a[i + 1]) * a[i + 1];
+      sum += mint(a[i]) * a[i];
+      sum *= a[i];
+      ans += sum;
+    }
+    return int(ans);
+  }
+};
