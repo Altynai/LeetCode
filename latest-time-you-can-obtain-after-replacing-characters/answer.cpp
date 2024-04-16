6bb43ad8f92d18d2f2c9602cb19a5ae89f335b45
Jiapeng Chen <jiapeng.chen@uber.com>
2024-04-16 08:46:38 +0200

    Add Solution: Latest Time You Can Obtain After Replacing Characters

---
+++ latest-time-you-can-obtain-after-replacing-characters/answer.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+  string findLatestTime(string s) {
+    if (s[0] == '?') {
+      if (s[1] == '?')
+        s[0] = s[1] = '1';
+      else if (s[1] >= '2')
+        s[0] = '0';
+      else
+        s[0] = '1';
+    }
+
+    if (s[1] == '?') {
+      if (s[0] == '0')
+        s[1] = '9';
+      else
+        s[1] = '1';
+    }
+
+    if (s[3] == '?')
+      s[3] = '5';
+    if (s[4] == '?')
+      s[4] = '9';
+
+    return s;
+  }
+};
