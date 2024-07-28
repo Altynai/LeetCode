0b35d275136600cf1a097d96304b565ca95376cf
Jiapeng Chen <jiapeng.chen@uber.com>
2024-07-28 11:29:54 +0200

    Add Solution: Find the Count of Numbers Which Are Not Special

---
+++ find-the-count-of-numbers-which-are-not-special/answer.cpp
@@ -0,0 +1,61 @@
+struct PrimeFactorization {
+
+  // primes.size() <= sqrt(n)
+  vector<int> primes;
+  vector<bool> p;
+
+  PrimeFactorization(int n) {
+    p.resize(n);
+    p.assign(n, true);
+
+    p[0] = p[1] = 0;
+    for (int i = 4; i < n; i += 2)
+      p[i] = 0;
+    for (int i = 3; i * i < n; i += 2) {
+      if (!p[i])
+        continue;
+      for (int j = i * i; j < n; j += i)
+        p[j] = 0;
+    }
+
+    primes = {2};
+    for (int i = 3; i < n; i += 2)
+      if (p[i])
+        primes.push_back(i);
+  }
+
+  vector<pair<int, int>> getFactors(int n) {
+    vector<pair<int, int>> ans;
+    for (int i = 0; n > 1 && i < (int)primes.size(); i++) {
+      int cnt = 0;
+      while (n % primes[i] == 0) {
+        n /= primes[i];
+        cnt++;
+      }
+      if (cnt)
+        ans.emplace_back(primes[i], cnt);
+    }
+    if (n > 1)
+      ans.emplace_back(n, 1);
+    return ans;
+  }
+} pf(31634);
+
+class Solution {
+private:
+  int count(int n) {
+    int ans = 0;
+    for (int x : pf.primes) {
+      if (x * x > n)
+        break;
+      ans++;
+    }
+    return ans;
+  }
+
+public:
+  int nonSpecialCount(int l, int r) {
+    int special = count(r) - count(l - 1);
+    return r - l + 1 - special;
+  }
+};
