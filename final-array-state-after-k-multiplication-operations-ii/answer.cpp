6fbd7f11be68728c754663375112ae3cd49643db
Jiapeng Chen <jiapeng.chen@uber.com>
2024-08-25 18:34:51 +0200

    Add Solution: Final Array State After K Multiplication Operations II

---
+++ final-array-state-after-k-multiplication-operations-ii/answer.cpp
@@ -0,0 +1,167 @@
+template <int MOD_> struct modnum {
+  typedef long long int64;
+
+  static constexpr int MOD = MOD_;
+  static_assert(MOD_ > 0, "MOD must be positive");
+
+private:
+  int v;
+  static int minv(int a, int m) {
+    a %= m;
+    assert(a);
+    return a == 1 ? 1 : int(m - int64(minv(m, a)) * int64(m) / a);
+  }
+
+public:
+  modnum() : v(0){};
+  modnum(int64 v_) : v(int(v_ % MOD)) {
+    if (v < 0)
+      v += MOD;
+  }
+  explicit operator int() const { return v; }
+  friend std::ostream &operator<<(std::ostream &out, const modnum &n) {
+    return out << int(n);
+  }
+  friend std::istream &operator>>(std::istream &in, modnum &n) {
+    int64 v_;
+    in >> v_;
+    n = modnum(v_);
+    return in;
+  }
+  friend bool operator==(const modnum &a, const modnum &b) {
+    return a.v == b.v;
+  }
+  friend bool operator!=(const modnum &a, const modnum &b) {
+    return a.v != b.v;
+  }
+
+  modnum inv() const {
+    modnum res;
+    res.v = minv(v, MOD);
+    return res;
+  }
+  friend modnum inv(const modnum &m) { return m.inv(); }
+  modnum neg() const {
+    modnum res;
+    res.v = v ? MOD - v : 0;
+    return res;
+  }
+  friend modnum neg(const modnum &m) { return m.neg(); }
+
+  modnum pow(int64 m) {
+    assert(m >= 0);
+    modnum res = 1, p = modnum(*this);
+    while (m) {
+      if (m & 1)
+        res *= p;
+      p *= p;
+      m /= 2;
+    }
+    return res;
+  }
+
+  modnum operator-() const { return neg(); }
+  modnum operator+() const { return modnum(*this); }
+
+  modnum &operator++() {
+    ++v;
+    if (v == MOD)
+      v = 0;
+    return *this;
+  }
+  modnum &operator--() {
+    if (v == 0)
+      v = MOD;
+    --v;
+    return *this;
+  }
+  modnum &operator+=(const modnum &o) {
+    v += o.v;
+    if (v >= MOD)
+      v -= MOD;
+    return *this;
+  }
+  modnum &operator-=(const modnum &o) {
+    v -= o.v;
+    if (v < 0)
+      v += MOD;
+    return *this;
+  }
+  modnum &operator*=(const modnum &o) {
+    v = int(int64(v) * int64(o.v) % MOD);
+    return *this;
+  }
+  modnum &operator/=(const modnum &o) { return *this *= o.inv(); }
+
+  friend modnum operator++(modnum &a, int) {
+    modnum r = a;
+    ++a;
+    return r;
+  }
+  friend modnum operator--(modnum &a, int) {
+    modnum r = a;
+    --a;
+    return r;
+  }
+  friend modnum operator+(const modnum &a, const modnum &b) {
+    return modnum(a) += b;
+  }
+  friend modnum operator-(const modnum &a, const modnum &b) {
+    return modnum(a) -= b;
+  }
+  friend modnum operator*(const modnum &a, const modnum &b) {
+    return modnum(a) *= b;
+  }
+  friend modnum operator/(const modnum &a, const modnum &b) {
+    return modnum(a) /= b;
+  }
+};
+using mint = modnum<1000000007>;
+// using mint = modnum<998244353>;
+
+using int64 = long long;
+
+template <typename T>
+using min_priority_queue = priority_queue<T, vector<T>, greater<T>>;
+
+class Solution {
+public:
+  vector<int> getFinalState(vector<int> &a, int k, int multiplier) {
+    if (multiplier == 1)
+      return a;
+
+    int n = a.size();
+
+    int64 max_value = *max_element(a.begin(), a.end());
+    min_priority_queue<pair<int, int>> que;
+    for (int i = 0; i < n; i++)
+      que.push({a[i], i});
+
+    while (k > 0 and !que.empty() and
+           1LL * que.top().first * multiplier <= max_value) {
+      auto [_, pos] = que.top();
+      que.pop();
+      a[pos] *= multiplier;
+      k--;
+      que.push({a[pos], pos});
+    }
+
+    // now min_value * multiplier > max_value
+    // which indicates it's just a looping process now by order
+    vector<int> orders;
+    while (!que.empty()) {
+      orders.push_back(que.top().second);
+      que.pop();
+    }
+
+    vector<int> ans = a;
+    for (int i = 0; i < n; i++) {
+      int j = orders[i];
+      int times = k / n;
+      if (k % n != 0 and i < k % n)
+        times++;
+      ans[j] = int(mint(multiplier).pow(times) * ans[j]);
+    }
+    return ans;
+  }
+};
