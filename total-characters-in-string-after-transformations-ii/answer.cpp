d39a30d9834b7c02b4b00013890f7eb575bd462b
Jiapeng Chen <jiapeng.chen@uber.com>
2024-10-27 10:07:03 +0100

    Add Solution: Total Characters in String After Transformations II

---
+++ total-characters-in-string-after-transformations-ii/answer.cpp
@@ -0,0 +1,187 @@
+template <int MOD_> struct modnum {
+  typedef long long int64;
+
+  static constexpr int MOD = MOD_;
+  static_assert(MOD_ > 0, "MOD must be positive");
+
+private:
+  int v;
+  static int minv(int a, int m) {
+    a %= m;
+    assert(a);
+    return a == 1 ? 1 : int(m - int64(minv(m, a)) * int64(m) / a);
+  }
+
+public:
+  modnum() : v(0){};
+  modnum(int64 v_) : v(int(v_ % MOD)) {
+    if (v < 0)
+      v += MOD;
+  }
+  explicit operator int() const { return v; }
+  friend std::ostream &operator<<(std::ostream &out, const modnum &n) {
+    return out << int(n);
+  }
+  friend std::istream &operator>>(std::istream &in, modnum &n) {
+    int64 v_;
+    in >> v_;
+    n = modnum(v_);
+    return in;
+  }
+  friend bool operator==(const modnum &a, const modnum &b) {
+    return a.v == b.v;
+  }
+  friend bool operator!=(const modnum &a, const modnum &b) {
+    return a.v != b.v;
+  }
+
+  modnum inv() const {
+    modnum res;
+    res.v = minv(v, MOD);
+    return res;
+  }
+  friend modnum inv(const modnum &m) { return m.inv(); }
+  modnum neg() const {
+    modnum res;
+    res.v = v ? MOD - v : 0;
+    return res;
+  }
+  friend modnum neg(const modnum &m) { return m.neg(); }
+
+  modnum pow(int64 m) {
+    assert(m >= 0);
+    modnum res = 1, p = modnum(*this);
+    while (m) {
+      if (m & 1)
+        res *= p;
+      p *= p;
+      m /= 2;
+    }
+    return res;
+  }
+
+  modnum operator-() const { return neg(); }
+  modnum operator+() const { return modnum(*this); }
+
+  modnum &operator++() {
+    ++v;
+    if (v == MOD)
+      v = 0;
+    return *this;
+  }
+  modnum &operator--() {
+    if (v == 0)
+      v = MOD;
+    --v;
+    return *this;
+  }
+  modnum &operator+=(const modnum &o) {
+    v += o.v;
+    if (v >= MOD)
+      v -= MOD;
+    return *this;
+  }
+  modnum &operator-=(const modnum &o) {
+    v -= o.v;
+    if (v < 0)
+      v += MOD;
+    return *this;
+  }
+  modnum &operator*=(const modnum &o) {
+    v = int(int64(v) * int64(o.v) % MOD);
+    return *this;
+  }
+  modnum &operator/=(const modnum &o) { return *this *= o.inv(); }
+
+  friend modnum operator++(modnum &a, int) {
+    modnum r = a;
+    ++a;
+    return r;
+  }
+  friend modnum operator--(modnum &a, int) {
+    modnum r = a;
+    --a;
+    return r;
+  }
+  friend modnum operator+(const modnum &a, const modnum &b) {
+    return modnum(a) += b;
+  }
+  friend modnum operator-(const modnum &a, const modnum &b) {
+    return modnum(a) -= b;
+  }
+  friend modnum operator*(const modnum &a, const modnum &b) {
+    return modnum(a) *= b;
+  }
+  friend modnum operator/(const modnum &a, const modnum &b) {
+    return modnum(a) /= b;
+  }
+};
+using mint = modnum<1000000007>;
+// using mint = modnum<998244353>;
+
+const int N = 26;
+
+template <typename T> struct Matrix {
+  T v[N][N];
+
+  Matrix() {
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++)
+        v[i][j] = 0;
+    }
+  }
+
+  void diagonal() {
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++)
+        v[i][j] = (i == j ? 1 : 0);
+    }
+  }
+
+  friend Matrix operator*(const Matrix &lh, const Matrix &rh) {
+    Matrix res;
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        T val = 0;
+        for (int k = 0; k < N; k++)
+          val += lh.v[i][k] * rh.v[k][j];
+        res.v[i][j] = val;
+      }
+    }
+    return res;
+  }
+};
+
+class Solution {
+public:
+  int lengthAfterTransformations(string s, int t, vector<int> &a) {
+    Matrix<mint> mat;
+    for (int i = 0; i < N; i++) {
+      for (int j = 1; j <= a[i]; j++) {
+        int k = (i + j) % N;
+        // coef contribution from i to k
+        mat.v[i][k] += 1;
+      }
+    }
+
+    Matrix<mint> res, p = mat;
+    res.diagonal();
+
+    for (; t > 0; t /= 2) {
+      if (t & 1)
+        res = res * p;
+      p = p * p;
+    }
+
+    vector<int> coef(N, 0);
+    for (char ch : s)
+      coef[ch - 'a']++;
+
+    mint ans = 0;
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++)
+        ans += res.v[j][i] * coef[j];
+    }
+    return int(ans);
+  }
+};
